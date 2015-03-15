12d05464de69885ce0511c774ff1988a96b0e270
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-03-14 21:26:30 -0500

    Initial config

---
+++ init.el
@@ -0,0 +1,81 @@
+;;;; Code:
+(require 'cask "/Users/juanpablosantos/.emacs.d/.cask/24.4.1/elpa/cask-20150109.621/cask.el")
+(cask-initialize)
+(require 'pallet)
+(pallet-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (monokai)))
+ '(custom-safe-themes
+   (quote
+    ("a041a61c0387c57bb65150f002862ebcfe41135a3e3425268de24200b82d6ec9" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+(require 'evil)
+(evil-mode 1)
+(setq scroll-step            1
+      scroll-conservatively  10000)
+
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+
+(setq scroll-step 1)
+
+;; Enable mouse support
+(unless window-system
+  (require 'mouse)
+  (xterm-mouse-mode t)
+  (global-set-key [mouse-4] '(lambda ()
+			       (interactive)
+			       (scroll-down 1)))
+  (global-set-key [mouse-5] '(lambda ()
+			       (interactive)
+			       (scroll-up 1)))
+  (defun track-mouse (e))
+  (setq mouse-sel-mode t)
+)
+
+(setq-default indent-tabs-mode nil)
+(setq tab-width 2)
+(global-linum-mode t)
+(column-number-mode 1)
+
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
+(toggle-truncate-lines)
+
+(require 'ido)
+(ido-mode t)
+
+(require 'flx-ido)
+(ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
+(setq ido-enable-flex-matching t)
+(setq ido-use-faces nil)
+
+(require 'escreen)
+(escreen-install)
+
+(menu-bar-mode -1)
+
+(set-face-attribute 'vertical-border
+                    nil
+                    :foreground "gray")
+;;;; Commentary
+
+(provide 'init)
+;;; init.el ends here
