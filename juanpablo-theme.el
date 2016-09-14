0017de27d27563bbaa1ee2898990b91ad2cbb963
Juan Pablo Santos <jsantos@stackbuilders.com>
2016-09-14 14:25:08 -0500

    Updates

---
+++ juanpablo-theme.el
@@ -0,0 +1,15 @@
+(deftheme juanpablo
+  "Created 2016-06-24.")
+
+(custom-theme-set-variables
+ 'juanpablo
+ '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
+ '(compilation-message-face (quote default))
+ '(custom-safe-themes (quote ("c59857e3e950131e0c17c65711f1812d20a54b829115b7c522672ae6ba0864cc" "6df30cfb75df80e5808ac1557d5cc728746c8dbc9bc726de35b15180fa6e0ad9" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default))))
+
+(custom-theme-set-faces
+ 'juanpablo
+ '(linum ((t (:inherit (default default))))))
+
+(provide-theme 'juanpablo)
