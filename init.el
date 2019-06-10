e16fcd0de37e3fc34b268153084dd10a7a20800b
Juan Pablo Santos <santos.juanpablo@gmail.com>
2019-06-10 17:43:06 -0500

    More 2019 updates

--- init.el
+++ init.el
@@ -9,7 +9,6 @@
 (require 'cask "/usr/local/Cellar/cask/0.8.4/cask.el")
 (cask-initialize)
 (require 'pallet)
-(require 'ensime)
 (pallet-mode t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -20,6 +19,7 @@
    [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector
    ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
+ '(beacon-color "#d33682")
  '(compilation-message-face (quote default))
  '(cua-global-mark-cursor-color "#2aa198")
  '(cua-normal-cursor-color "#657b83")
@@ -28,8 +28,10 @@
  '(custom-enabled-themes (quote (smart-mode-line-dark)))
  '(custom-safe-themes
    (quote
-    ("bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" "718fb4e505b6134cc0eafb7dad709be5ec1ba7a7e8102617d87d3109f56d9615" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "cc60d17db31a53adf93ec6fad5a9cfff6e177664994a52346f81f62840fe8e23" "fe243221e262fe5144e89bb5025e7848cd9fb857ff5b2d8447d115e58fede8f7" "15348febfa2266c4def59a08ef2846f6032c0797f001d7b9148f30ace0d08bcf" "f78de13274781fbb6b01afd43327a4535438ebaeec91d93ebdbba1e3fba34d3c" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+    ("04589c18c2087cd6f12c01807eed0bdaa63983787025c209b89c779c61c3a4c4" "9b35c097a5025d5da1c97dba45fed027e4fb92faecbd2f89c2a79d2d80975181" "8b4d8679804cdca97f35d1b6ba48627e4d733531c64f7324f764036071af6534" "13d20048c12826c7ea636fbe513d6f24c0d43709a761052adbca052708798ce3" "e61752b5a3af12be08e99d076aedadd76052137560b7e684a8be2f8d2958edc3" "d057f0430ba54f813a5d60c1d18f28cf97d271fd35a36be478e20924ea9451bd" "392395ee6e6844aec5a76ca4f5c820b97119ddc5290f4e0f58b38c9748181e8d" "26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" "11e57648ab04915568e558b77541d0e94e69d09c9c54c06075938b6abc0189d8" "bd7b7c5df1174796deefce5debc2d976b264585d51852c962362be83932873d9" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "2a739405edf418b8581dcd176aaf695d319f99e3488224a3c495cb0f9fd814e3" "718fb4e505b6134cc0eafb7dad709be5ec1ba7a7e8102617d87d3109f56d9615" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "8ed752276957903a270c797c4ab52931199806ccd9f0c3bb77f6f4b9e71b9272" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "cc60d17db31a53adf93ec6fad5a9cfff6e177664994a52346f81f62840fe8e23" "fe243221e262fe5144e89bb5025e7848cd9fb857ff5b2d8447d115e58fede8f7" "15348febfa2266c4def59a08ef2846f6032c0797f001d7b9148f30ace0d08bcf" "f78de13274781fbb6b01afd43327a4535438ebaeec91d93ebdbba1e3fba34d3c" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
  '(fci-rule-color "#49483E")
+ '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
+ '(frame-background-mode (quote dark))
  '(fringe-mode 6 nil (fringe))
  '(haskell-process-auto-import-loaded-modules t)
  '(haskell-process-log t)
@@ -59,6 +61,24 @@
     ("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3")))
  '(hl-paren-background-colors (quote ("#2492db" "#95a5a6" nil)))
  '(hl-paren-colors (quote ("#ecf0f1" "#ecf0f1" "#c0392b")))
+ '(hl-todo-keyword-faces
+   (quote
+    (("TODO" . "#dc752f")
+     ("NEXT" . "#dc752f")
+     ("THEM" . "#2d9574")
+     ("PROG" . "#4f97d7")
+     ("OKAY" . "#4f97d7")
+     ("DONT" . "#f2241f")
+     ("FAIL" . "#f2241f")
+     ("DONE" . "#86dc2f")
+     ("NOTE" . "#b1951d")
+     ("KLUDGE" . "#b1951d")
+     ("HACK" . "#b1951d")
+     ("TEMP" . "#b1951d")
+     ("FIXME" . "#dc752f")
+     ("XXX" . "#dc752f")
+     ("XXXX" . "#dc752f")
+     ("???" . "#dc752f"))))
  '(js2-basic-offset 2)
  '(js2-bounce-indent-p t)
  '(js2-strict-missing-semi-warning nil)
@@ -69,7 +89,7 @@
     ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
  '(package-selected-packages
    (quote
-    (counsel-projectile psc-ide elcord evil-surround molokai-theme color-theme-molokai ujelly-theme discord ## auctex nlinum flatland-theme flatui-dark-theme flatui-theme moe-theme zenburn-theme spacemacs-theme atom-one-dark-theme cherry-blossom-theme color-theme-heroku indent-guide intero xquery-mode popup-imenu hydra yaml-mode ws-butler web-mode transpose-frame toml-mode stylus-mode solarized-theme smart-mode-line ruby-electric ruby-additional rspec-mode rotate rainbow-delimiters purescript-mode projectile php-mode pallet osx-clipboard nummm-mode nix-mode neotree monokai-theme mmm-mode markdown-mode+ magit less-css-mode json-mode js3-mode jinja2-mode jade-mode hindent flycheck-tip flycheck-rust flycheck-purescript flycheck-haskell flx-ido evil-snipe evil-matchit ensime elixir-mode diminish csharp-mode company-ghc color-theme-sanityinc-solarized cider cargo buffer-move ag ac-js2 ac-haskell-process)))
+    (lsp-mode lsp-ui lsp-scala counsel-projectile psc-ide elcord evil-surround molokai-theme color-theme-molokai ujelly-theme discord ## auctex nlinum flatland-theme flatui-dark-theme flatui-theme moe-theme zenburn-theme spacemacs-theme atom-one-dark-theme cherry-blossom-theme color-theme-heroku indent-guide intero xquery-mode popup-imenu hydra yaml-mode ws-butler web-mode transpose-frame toml-mode stylus-mode solarized-theme smart-mode-line ruby-electric ruby-additional rspec-mode rotate rainbow-delimiters purescript-mode projectile php-mode pallet osx-clipboard nummm-mode nix-mode neotree monokai-theme mmm-mode markdown-mode+ magit less-css-mode json-mode js3-mode jinja2-mode jade-mode hindent flycheck-tip flycheck-rust flycheck-purescript flycheck-haskell flx-ido evil-snipe evil-matchit elixir-mode diminish csharp-mode company-ghc color-theme-sanityinc-solarized cider cargo buffer-move ag ac-js2 ac-haskell-process)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(pos-tip-background-color "#eee8d5")
  '(pos-tip-foreground-color "#586e75")
@@ -168,6 +188,7 @@
 (setq evil-split-window-below t)
 (setq evil-vsplit-window-right t)
 (setq max-specpdl-size 10000)
+(setq mode-require-final-newline t)
 
 (setq scroll-step            1
       scroll-conservatively  10000)
@@ -317,31 +338,6 @@
 (setq visible-bell nil)
 (setq ring-bell-function nil)
 
-(setq ensime-startup-notification nil)
-(setq ensime-startup-snapshot-notification nil)
-(setq ensime-sbt-command "./sbt")
-
-(with-eval-after-load 'ensime
-  (setq ensime-sem-high-faces
-        '(
-          (implicitConversion nil)
-          (var . (:foreground "#ff2222"))
-          (varField . (:foreground "#ff3333"))
-          (functionCall . (:foreground "#dc9157"))
-          (object . (:foreground "#D884E3"))
-          (operator . (:foreground "#cc7832"))
-          (object . (:foreground "#6897bb" :slant italic))
-          (package . (:foreground "yellow"))
-          (deprecated . (:strike-through "#a9b7c6"))
-          (implicitParams nil)
-          )
-        ensime-completion-style 'company
-        ensime-sem-high-enabled-p nil ;; disable semantic highlighting
-        ensime-tooltip-hints t ;; disable type-inspecting tooltips
-        ensime-tooltip-type-hints t ;; disable typeinspecting tooltips
-        )
-  )
-
 (setq exec-path (append exec-path '("/usr/local/bin")))
 
 (require 'psc-ide)
@@ -354,6 +350,54 @@
     (turn-on-purescript-indentation)))
 
 (setq psc-ide-use-npm-bin t)
+
+(evil-snipe-mode +1)
+(evil-snipe-override-mode +1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Enable defer and ensure by default for use-package
+;; (setq use-package-always-defer t
+;;       use-package-always-ensure t)
+
+;; ;; Enable scala-mode and sbt-mode
+;; (use-package scala-mode
+;;   :mode "\\.s\\(cala\\|bt\\)$")
+
+;; (use-package sbt-mode
+;;   :commands sbt-start sbt-command
+;;   :config
+;;   ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+;;   ;; allows using SPACE when in the minibuffer
+;;   (substitute-key-definition
+;;    'minibuffer-complete-word
+;;    'self-insert-command
+;;    minibuffer-local-completion-map))
+
+;; ;; Enable nice rendering of diagnostics like compile errors.
+;; (use-package flycheck
+;;   :init (global-flycheck-mode))
+
+;; (use-package lsp-mode
+;;  :init (setq lsp-prefer-flymake nil))
+
+;; (use-package lsp-ui
+;;   :hook (lsp-mode . lsp-ui-mode))
+
+;; (use-package lsp-scala
+;;   :after scala-mode
+;;   :demand t
+;;   ;; Optional - enable lsp-scala automatically in scala files
+;;   :init (setq lsp-scala-server-command "~/.local/bin/metals-emacs")
+;;   :hook (scala-mode . lsp))
+
+;; (use-package lsp-scala
+;;   :after scala-mode
+;;   :demand t
+;;   :hook (scala-mode . lsp)
+;;   )
 
 ;;;; Commentary
 
